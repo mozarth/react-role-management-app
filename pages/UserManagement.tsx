1816e7e1c78af1e36cd7e879f920b3db7e11282f
mozarth <mozarth735@gmail.com>
2025-07-21 15:59:03 -0500

    Add pages/UserManagement.tsx 1753131543404

---
+++ pages/UserManagement.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserManagementView from "@/components/users/UserManagementView";
+
+const UserManagement: React.FC = () => {
+  return <UserManagementView />;
+};
+
+export default UserManagement;
